8e82eb2221b43fbd2a9d10a125d8328103eef267
witekrz <rzeszutekwit@gmail.com>
2023-12-03 01:35:45 +0100

    day two, part 2 - java style :)

--- 2023/rust/day-02/src/part2.rs
+++ 2023/rust/day-02/src/part2.rs
@@ -1,10 +1,80 @@
+use itertools::Itertools;
+
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let output: Vec<Game> = input
+        .lines().map(|line| {
+        return Game::new(line);
+    }).collect();
+    let x: u32 = output.iter().map(|game| game.multiple()).sum();
+    return Ok(x.to_string());
+}
+
+struct Game {
+    pub id: u32,
+    cube_sets: Vec<CubeSet>,
+}
+
+impl Game {
+    pub fn new(line: &str) -> Game {
+        let colon_split: Vec<&str> = line.split(':').collect();
+        let full_line_values = colon_split.last().unwrap();
+        let cube_sets: Vec<&str> = full_line_values.split(';').collect();
+        let color_value_vec: Vec<CubeSet> = cube_sets.iter()
+            .map(|value| {
+                let values: Vec<&str> = value.split(',').collect();
+                let cos: Vec<PairColorValue> = values.iter().map(|cv| {
+                    let color_value_t: Vec<&str> = cv.trim().split(' ').collect();
+                    let color: &str = color_value_t.last().unwrap();
+                    let value: &str = color_value_t.first().unwrap();
+                    return PairColorValue { color: color.to_string(), value: value.parse::<u32>().unwrap() };
+                }).collect();
+                return CubeSet { color_value: cos };
+            }).collect();
+        let idf: Vec<&str> = colon_split.first().unwrap().split(' ').collect();
+        Game { id: idf.last().unwrap().parse::<u32>().unwrap(), cube_sets: color_value_vec }
+    }
+    pub fn is_ok(&self) -> bool {
+        return self.cube_sets.iter().all(|cs| cs.is_ok());
+    }
+    pub fn multiple(&self) -> u32 {
+        let z: Vec<PairColorValue> = self.cube_sets.clone().into_iter().map(|z| z.color_value).flatten().collect();
+        let max_red = z.iter().filter(|a| a.color == "red").map(|b| b.value).max().unwrap();
+        let max_green = z.iter().filter(|a| a.color == "green").map(|b| b.value).max().unwrap();
+        let max_blue = z.iter().filter(|a| a.color == "blue").map(|b| b.value).max().unwrap();
+        max_red * max_blue * max_green
+    }
+}
+
+#[derive(Debug, Clone)]
+struct PairColorValue {
+    color: String,
+    value: u32,
+}
+
+impl PairColorValue {
+    pub fn is_ok(&self) -> bool {
+        return if (self.color == "blue" && self.value < 15)
+            || (self.color == "red" && self.value < 13)
+            || (self.color == "green" && self.value < 14) {
+            true
+        } else {
+            false
+        };
+    }
+}
+
+#[derive(Debug, Clone)]
+struct CubeSet {
+    color_value: Vec<PairColorValue>,
+}
+
+impl CubeSet {
+    pub fn is_ok(&self) -> bool {
+        self.color_value.iter().all(|cv| cv.is_ok())
+    }
 }
 
 #[cfg(test)]
@@ -13,9 +83,12 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        assert_eq!("2286", process(input)?);
         Ok(())
     }
 }
